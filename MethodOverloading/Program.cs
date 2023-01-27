6552df2830ee7cee80455142b8e4220da124ae91
Celeste McCall <celestemccall93@gmail.com>
2023-01-26 21:28:50 -0600

    inital commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,47 @@
 {
     public class Program
     {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static double Add(double c, double d)
+        {
+            return (c + d);
+        }
+
+        public static string Add(int e, int f, bool isTrue)
+        {
+            var sum = e + f;
+           
+
+            if (isTrue == true && sum > 1)
+            {
+                return  ($"{sum} dollars");
+                
+            }
+            else if ( isTrue == true && sum == 1)
+            {
+                return  ($"{sum} dollar");
+            }
+            else if (isTrue == true && sum < 1)
+            {
+                return ($"{sum} dollars");
+            }
+            else
+            {
+                return sum.ToString();
+            }
+            
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(15 , 17));
+            Console.WriteLine(Add(0.7 , 0.9));
+            Console.WriteLine(Add(0 , 0 , false));
         }
     }
 }
